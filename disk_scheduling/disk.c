c427de86bda138a34f097c7b9d9e4e6f5b4bd7e3
fernwired <figonzalez@proton.me>
2025-09-22 16:34:17 +0000

    rename main.c to disk.c for disk_scheduling. import the oslabs.h to this folder too.

---
+++ disk_scheduling/disk.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "oslabs.h"
+
+int main() {
+  // TODO: Implement disk scheduling algorithms
+  printf("Disk scheduling algorithms not implemented yet.\n");
+  return 0;
+}
